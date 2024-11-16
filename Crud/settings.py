8ba79cddff0d1ffdfe6a0ce89fd33da0c629ddac
maralimjanv <umarolimjonov2009@gmail.com>
2024-11-16 06:58:44 +0500

    conneting files

--- Crud/settings.py
+++ Crud/settings.py
@@ -37,8 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'crud_items',
     'rest_framework',
+    'book',
+
 
 ]
 
